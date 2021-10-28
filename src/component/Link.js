fee125c0fc2ac786820aef1987e6ad25438f3b71
drewjya <andre.wijaya004@binus.ac.id>
2021-10-28 20:29:03 +0700

    upd

--- src/component/Link.js
+++ src/component/Link.js
@@ -3,14 +3,14 @@ import {BsInstagram, BsWhatsapp, BsLinkedin, BsGithub} from 'react-icons/bs';
 
 function Link(props) {
     if(props.name==='wa'){
-        return (<a target="_blank" className="text-green-500" href="https://wa.me/628516944890"><ContactIcon icon={<BsWhatsapp size="68"/>}/></a>);
+        return (<a target="_blank" className="text-green-500 dark:text-green-500" href="https://wa.me/628516944890"><ContactIcon icon={<BsWhatsapp size="68"/>}/></a>);
     } else if(props.name==='github'){
-        return (<a target="_blank" className="text-gray-700" href="https://github.com/drewjya"><ContactIcon icon={<BsGithub size="68"/>}/></a>);
+        return (<a target="_blank" className="text-gray-700 dark:text-gray-200" href="https://github.com/drewjya"><ContactIcon icon={<BsGithub size="68"/>}/></a>);
     }else if(props.name==='linked'){
-        return (<a target="_blank" className="text-blue-800" href="https://id.linkedin.com/in/andre-wijaya-a22a5620b?trk=people-guest_people_search-card"><ContactIcon icon={<BsLinkedin size="68"/>}/></a>);
+        return (<a target="_blank" className="text-blue-800 dark:text-blue-900" href="https://id.linkedin.com/in/andre-wijaya-a22a5620b?trk=people-guest_people_search-card"><ContactIcon icon={<BsLinkedin size="68"/>}/></a>);
     }else if(props.name==='ig'){
         return (
-        <a className="text-white" target="_blank" href="https://www.instagram.com/drewjya/">
+        <a className="text-white dark:text-black" target="_blank" href="https://www.instagram.com/drewjya/">
             <BsInstagram className=" bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl"  size="68"/>
         </a>);
     }
