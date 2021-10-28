75896b61f593f6d40bbd7e748e6965529c5f683e
drewjya <andre.wijaya004@binus.ac.id>
2021-10-28 20:31:16 +0700

    upd

--- src/component/Link.js
+++ src/component/Link.js
@@ -5,9 +5,9 @@ function Link(props) {
     if(props.name==='wa'){
         return (<a target="_blank" className="text-green-500 dark:text-green-500" href="https://wa.me/628516944890"><ContactIcon icon={<BsWhatsapp size="68"/>}/></a>);
     } else if(props.name==='github'){
-        return (<a target="_blank" className="text-gray-700 dark:text-gray-200" href="https://github.com/drewjya"><ContactIcon icon={<BsGithub size="68"/>}/></a>);
+        return (<a target="_blank" className="text-gray-700 dark:text-black" href="https://github.com/drewjya"><ContactIcon icon={<BsGithub size="68"/>}/></a>);
     }else if(props.name==='linked'){
-        return (<a target="_blank" className="text-blue-800 dark:text-blue-900" href="https://id.linkedin.com/in/andre-wijaya-a22a5620b?trk=people-guest_people_search-card"><ContactIcon icon={<BsLinkedin size="68"/>}/></a>);
+        return (<a target="_blank" className="text-blue-800 dark:text-blue-300" href="https://id.linkedin.com/in/andre-wijaya-a22a5620b?trk=people-guest_people_search-card"><ContactIcon icon={<BsLinkedin size="68"/>}/></a>);
     }else if(props.name==='ig'){
         return (
         <a className="text-white dark:text-black" target="_blank" href="https://www.instagram.com/drewjya/">
